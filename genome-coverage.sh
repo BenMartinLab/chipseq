b5fc24647c9012d6831f7bbe5e17ae17476ea78f
Christian Poitras <christian.poitras@ircm.qc.ca>
2025-12-11 18:21:07 -0500

    Adds genome-coverage.sh script.

---
+++ genome-coverage.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+#SBATCH --account=def-bmartin
+#SBATCH --time=2:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4G
+#SBATCH --output=genome-coverage-%A_%a.out
+
+# exit when any command fails
+set -e
+
+if [[ -n "$CC_CLUSTER" ]]
+then
+  module purge
+  module load StdEnv/2023
+  module load bedtools/2.31.0
+  echo
+fi
+
+index=${SLURM_ARRAY_TASK_ID:-0}
+index=$((index+1))
+threads=${SLURM_CPUS_PER_TASK:-1}
+tmpdir=${SLURM_TMPDIR:-/tmp}
+
+samplesheet=samplesheet.csv
+genome=hg38.chrom.sizes
+output=output/bowtie2/merged_library
+scales_column=3
+suffix=.depth_scaled
+
+# Usage function
+usage() {
+  echo
+  echo "Usage: genome-coverage.sh [-i int] [-s samplesheet.csv] [-g hg38.chrom.sizes] " \
+       "[-o output/star_salmon] [-S scale-factors.txt] [-c 3] [-f .depth_scaled]"
+  echo "  -i: Index of sample in samplesheet (default: 1 or SLURM_ARRAY_TASK_ID+1 if present)"
+  echo "  -s: Samplesheet file (default: samplesheet.csv)"
+  echo "  -g: Genome chromosome sizes file (default: hg38.chrom.sizes)"
+  echo "  -o: Output folder where BAM files are located (default: output/bowtie2/merged_library)"
+  echo "  -S: File containing scale factors (default: \$output/scale-factors.txt)"
+  echo "  -c: File containing scale factors (default: 3)"
+  echo "  -f: Output file suffix (default: .depth_scaled)"
+  echo "  -h: Show this help"
+}
+
+# Parsing arguments.
+while getopts 'i:s:g:o:S:c:f:h' OPTION; do
+  case "$OPTION" in
+    i)
+       index="$OPTARG"
+       ;;
+    s)
+       samplesheet="$OPTARG"
+       ;;
+    g)
+       genome="$OPTARG"
+       ;;
+    o)
+       output="$OPTARG"
+       ;;
+    S)
+       scales="$OPTARG"
+       ;;
+    c)
+       scales_column="$OPTARG"
+       ;;
+    f)
+       suffix="$OPTARG"
+       ;;
+    h)
+       usage
+       exit 0
+       ;;
+    :)
+       usage
+       exit 1
+       ;;
+    ?)
+       usage
+       exit 1
+       ;;
+  esac
+done
+
+if [[ -z "$scales" ]]
+then
+  scales="$output"/scale-factors.txt
+fi
+
+# Validating arguments.
+if ! [[ "$index" =~ ^[0-9]+$ ]]
+then
+  >&2 echo "Error: -i parameter '$index' is not an integer."
+  usage
+  exit 1
+fi
+if ! [[ -f "$samplesheet" ]]
+then
+  >&2 echo "Error: -s file parameter '$samplesheet' does not exists."
+  usage
+  exit 1
+fi
+if ! [[ -f "$genome" ]]
+then
+  >&2 echo "Error: -g file parameter '$genome' does not exists."
+  usage
+  exit 1
+fi
+if ! [[ -d "$output" ]]
+then
+  >&2 echo "Error: -o folder parameter '$output' does not exists."
+  usage
+  exit 1
+fi
+if ! [[ -f "$scales" ]]
+then
+  >&2 echo "Error: -S file parameter '$scales' does not exists."
+  usage
+  exit 1
+fi
+if ! [[ "$scales_column" =~ ^[0-9]+$ ]]
+then
+  >&2 echo "Error: -C parameter '$scales_column' is not an integer."
+  usage
+  exit 1
+fi
+
+
+sample=$(awk -F ',' -v sample_index="$index" \
+    'NR > 1 && !seen[$1"_REP"$4] {ln++; seen[$1"_REP"$4]++; if (ln == sample_index) {print $1"_REP"$4}}' "$samplesheet")
+sample="${sample%%[[:cntrl:]]}"
+scale=$(awk -v sample="$sample" -v print_column="$scales_column" \
+    '$1 == sample {print $print_column}' "$scales")
+
+if [ "$scale" == "" ] || [ "$scale" == "NA" ]
+then
+  >&2 echo "Error: scale factor for sample '$sample' is '$scale' and coverage cannot be computed, exiting..."
+  exit 1
+fi
+
+bam="${output}/${sample}.mLb.clN.sorted.bam"
+if [[ ! -f "$bam" ]]
+then
+  >&2 echo "Error: BAM file '${sample}.mLb.clN.sorted.bam' does not exists in output folder '$output', exiting..."
+  exit 1
+fi
+
+echo "Running bedtools genomecov for sample $sample on BAM $bam"
+bedtools genomecov \
+    -bg \
+    -scale "$scale" \
+    -pc \
+    -ibam "$bam" \
+    | LC_ALL=C sort --parallel="$threads" -k1,1 -k2,2n \
+    > "${tmpdir}/${sample}.bedGraph"
+bedGraphToBigWig \
+    "${tmpdir}/${sample}.bedGraph" \
+    "$genome" \
+    "${output}/bigwig/${sample}${suffix}.bigWig"
