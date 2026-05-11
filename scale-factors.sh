12bc2206bfa9ad686dc7616c3412f4358b715b98
Christian Poitras <christian.poitras@ircm.qc.ca>
2026-05-11 10:12:11 -0400

    Improves how script name and path are obtained

--- scale-factors.sh
+++ scale-factors.sh
@@ -5,10 +5,13 @@
 #SBATCH --mem=4G
 #SBATCH --output=scale-factors-%A.out
 
+script_name=$(basename "${BASH_SOURCE[0]}")
 script_path=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
-if ! [[ -f "${script_path}/scale-factors.sh" ]] && [[ -n "$SLURM_JOB_ID" ]]
+if ! [[ -f "${script_path}/${script_name}" ]] && [[ -n "$SLURM_JOB_ID" ]]
 then
-  script_path=$(dirname "$(scontrol show job "$SLURM_JOB_ID" | awk -F '=' '$0 ~ /Command=/ {print $2; exit}')")
+  slurm_command=$(scontrol show job "$SLURM_JOB_ID" | awk -F '=' '$0 ~ /Command=/ {print $2; exit}')
+  script_name=$(basename "$slurm_command")
+  script_path=$(dirname "$slurm_command")
 fi
 source "${script_path}/nfcore-env/bin/activate"
 
